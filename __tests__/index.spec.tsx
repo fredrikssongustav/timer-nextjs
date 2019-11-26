0113d95663f0ae11aedaf679a61ee59aaf1a8911
Gustav Fredriksson <gustav.fredriksson@tink.se>
2019-11-26 11:32:59 +0100

    ref: don't store pages in folders, causing weird behaviour

--- __tests__/index.spec.tsx
+++ __tests__/index.spec.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { render, fireEvent } from '@testing-library/react'
-import IndexPage, { IndexForm } from '../pages/index/index'
+import IndexPage, { IndexForm } from '../pages/index'
 
 describe('Pages', () => {
 
---
+++ pages/clock.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import * as React from 'react';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
+const ClockPage: React.FC = () => {
+    const router = useRouter();
+    return (<div>
+
+    </div>)
+}
+
+export default ClockPage
