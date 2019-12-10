f086044f0ff3c44dc26f251fa582c3e2d4049c0a
Gustav Fredriksson <gustav.fredriksson@tink.se>
2019-12-10 10:52:32 +0100

    feat(clock): a demo clock that knows something countable

--- pages/clock.tsx
+++ pages/clock.tsx
@@ -1,15 +1,29 @@
 import Head from 'next/head';
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Router, { useRouter } from 'next/router';
 
-const Clock: React.FC = () => {
+export enum TIME_UNIT {
+    h = "h",
+    s = "s",
+    d = "d",
+    y = "y"
+}
+
+export type ClockProps = {
+    unit: TIME_UNIT | undefined,
+    value: number | undefined
+}
+
+export const Clock: React.FC<ClockProps> = ({ unit, value }) => {
     return (<div data-testid="clock" />);
 }
 
 const ClockPage: React.FC = () => {
     const router = useRouter();
-
+    // TODO: Don't set default values like this
+    const query = router ? router.query : { value: 10, unit: TIME_UNIT.s }
+    const { value, unit } = query
 
     const goBack = (event: React.MouseEvent<HTMLElement>) => {
         event.preventDefault()
@@ -19,7 +33,7 @@ const ClockPage: React.FC = () => {
     }
 
     return (<div>
-        <Clock />
+        <Clock unit={unit as TIME_UNIT | undefined} value={Number(value) ? Number(value) : undefined} />
         <button data-testid="start-button">
             start button
         </button>
