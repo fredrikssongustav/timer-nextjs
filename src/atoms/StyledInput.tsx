80bbc6765159a200226f2eb6e78e8d7b47b2b36a
Gustav Fredriksson <gustav.fredriksson@tink.se>
2019-12-14 17:27:22 +0100

    style: add MUI components to codebase

---
+++ src/atoms/StyledInput.tsx
@@ -0,0 +1,9 @@
+import { Input } from '@material-ui/core';
+import styled from 'styled-components';
+import * as React from 'react';
+
+const ThemedInput = styled(Input)`
+margin:10px;
+`
+
+export const StyledInput = ({ ...props }) => <ThemedInput {...props} />;
